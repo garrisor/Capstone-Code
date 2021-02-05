8e3afdcb6e9c2bd2691b63fc893a19cc854f0e1a
ronan <garrisor@mcmaster.ca>
2021-02-05 09:17:04 -0500

    Fixed icons

--- App.js
+++ App.js
@@ -1,114 +1,184 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+//import LiveScreen from './src/live';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+
+
+function HomeScreen() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.background}>
+      <View style={styles.displayBox1}>
+        <Text style={styles.displayBoxTitle}>Hourly Range</Text>
+        <Text style={styles.displayBoxDataText}>10 &deg;</Text>
+      </View>
+      <View style={styles.displayBox1}>
+        <Text style={styles.displayBoxTitle}>Daily Range</Text>
+        <Text style={styles.displayBoxDataText}>8 &deg;</Text>
+      </View>
+      <View style={styles.displayBox1}>
+        <Text style={styles.displayBoxTitle}>Weekly Range</Text>
+        <Text style={styles.displayBoxDataText}>9 &deg;</Text>
+      </View>
+    </View>
   );
-};
+}
+
+function HistoryScreen() {
+  return (
+    <View style={styles.background}>
+      <Text>History!</Text>
+    </View>
+  );
+}
+
+
+function LiveScreen() {
+  return (
+    <View style={styles.background}>
+      <View style={styles.graphBox}>
+        <Text>Dosiflexion curve will go here.</Text>
+      </View>
+      <View style={styles.displayBox2}>
+        <Text style={styles.displayBoxTitle}>Live Range</Text>
+        <Text style={styles.displayBoxDataText}>9 &deg;</Text>
+      </View>
+      <View style={styles.settingsBoxToggle}>
+        <Text>Live audio feedback?</Text>
+      </View>
+    </View>
+  );
+}
+
+
+function SettingsScreen() {
+  return (
+    <View style={styles.background}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator
+    initialRouteName="Home"
+    activeColor="#000000"
+    inactiveColor="#000000"
+    barStyle={{ backgroundColor: '#ffffff' }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="History"
+          component={HistoryScreen}
+
+          options={{
+          tabBarLabel: 'History',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="calendar" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Live"
+          component={LiveScreen}
+          options={{
+          tabBarLabel: 'Live',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="camera-timer" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+
+          options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  titleText: {
+    backgroundColor: '#ffffff',
+    fontFamily: "Cochin",
+    color: '#0384fc'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  displayBoxTitle: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    color: '#0384fc'
   },
-  body: {
-    backgroundColor: Colors.white,
+  displayBoxDataText: {
+    fontSize: 30,
+    color: '#0384fc'
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  background: {
+    flex: 1,
+    backgroundColor: '#0384fc',
+    justifyContent: "space-around",
+    alignItems: "center"
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  displayBox1: {
+    width: '75%',
+    height: '20%',
+    borderRadius: 25,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  displayBox2: {
+    width: '80%',
+    height: '20%',
+    borderRadius: 25,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  graphBox: {
+    width: '80%',
+    height: '40%',
+    borderRadius: 25,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    //position: 'absolute',
+    //top: 60
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  settingsBoxToggle: {
+    width: '80%',
+    height: '10%',
+    borderRadius: 25,
+    backgroundColor: '#364d5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+    //position: 'absolute',
+    //top: 60
+  }
 });
-
-export default App;
